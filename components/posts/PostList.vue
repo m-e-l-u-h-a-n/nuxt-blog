fcf62efb75313d80b315ecfd8b023d1123d9f6e0
m-e-l-u-h-a-n <pururshottam.tiwari.cd.cse19@itbhu.ac.in>
2020-08-08 12:16:14 +0530

    post related components work

---
+++ components/posts/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="featured-posts">
+	  <post-preview id="1" title="Hello there" previewText="This is my first post!"/>
+	  <post-preview id="2" title="bello there" previewText="This is my second post!"/>
+	  <post-preview id="3" title="tello there" previewText="This is my third post!"/>
+	</section>
+</template>
+<script>
+import PostPreview from '@/components/posts/PostPreview';
+export default {
+	components: {
+		PostPreview
+	}
+}
+</script>
+<style scoped>
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
