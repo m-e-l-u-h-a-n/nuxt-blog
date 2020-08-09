a10377b6583654ce7ef679746a47e229c5d73f9d
m-e-l-u-h-a-n <pururshottam.tiwari.cd.cse19@itbhu.ac.in>
2020-08-09 12:06:29 +0530

    firebase fetch and post implemented

--- components/admin/AdminPostForm.vue
+++ components/admin/AdminPostForm.vue
@@ -2,8 +2,10 @@
     <form @submit.prevent="onSave">
         <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
         <AppControlInput v-model="editedPost.title">Title</AppControlInput>
-        <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+        <AppControlInput v-model="editedPost.thumbnail">Thumbnail Link</AppControlInput>
         <AppControlInput control-type="textarea" v-model="editedPost.content">Content</AppControlInput>
+        <AppControlInput control-type="textarea" v-model="editedPost.previewText">Preview Text:</AppControlInput>
+
         <AppButton type="submit">Save</AppButton>
         <AppButton
           type="button"
@@ -28,15 +30,16 @@ export default {
             editedPost: this.post?{...this.post}:{
                 author: '',
                 title: '',
-                thumbnailLink: '',
+                thumbnail: '',
                 content: '',
+                previewText: ''
             }
         };
     },
     methods: {
         onSave(){
             //on save
-            console.log(this.editedPost);
+            this.$emit('submit',this.editedPost);
         },
         onCancel(){
             //navigate back
--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 import Vuex from 'vuex'
-
+import axios from 'axios'
 const createStore = () => {
     return new Vuex.Store({
         state: {
@@ -10,17 +10,24 @@ const createStore = () => {
                 state.loadedPosts = posts
             }
         },
-        actions: {
-            setPosts(vuexContext, posts) {
-                vuexContext.commit('setPosts', posts)
-            }
-        },
         getters: {
             loadedPosts(state) {
                 return state.loadedPosts
             }
-        }
+        },
+        actions: {
+            nuxtServerInit(vuexContext, context) {
+                return axios.get("https://nuxt-blog-fcd31.firebaseio.com/posts.json")
+                    .then(res => {
+                        console.log(res.data);
+                        const postsArray = [];
+                        for (const key in res.data) {
+                            postsArray.push({...res.data[key], id: key })
+                        }
+                        vuexContext.commit('setPosts', postsArray)
+                    }).catch(e => context.error(e))
+            },
+        },
     })
 }
-
 export default createStore
